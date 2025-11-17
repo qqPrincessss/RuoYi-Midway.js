522b942f012530c207cfa441f45e1f1a9037bb66
xierfloat <2053619887@qq.com>
2025-11-18 01:20:56 +0800

    feat:vue

--- frontend-vue3/src/store/modules/user.js
+++ frontend-vue3/src/store/modules/user.js
@@ -33,7 +33,7 @@ const useUserStore = defineStore(
       getInfo() {
         return new Promise((resolve, reject) => {
           getInfo().then(res => {
-            const user = res.user
+            const user = res.data.user;
             const avatar = (user.avatar == "" || user.avatar == null) ? defAva : import.meta.env.VITE_APP_BASE_API + user.avatar;
 
             if (res.roles && res.roles.length > 0) { // 验证返回的roles是否是一个非空数组
